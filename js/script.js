d9c25c0374ccc1b0a0775c46d51f4fd0834df62d
BornInSuriname <fvroom@live.com>
2025-03-23 15:33:34 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
-
-// Wacht tot de pagina volledig geladen is
 document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.querySelector(".menu-toggle");
     const navLinks = document.querySelector(".nav-links");
 
     if (menuToggle && navLinks) {
         menuToggle.addEventListener("click", function () {
-            navLinks.classList.toggle("active"); // Toggle het menu aan/uit
+            navLinks.classList.toggle("active");
         });
 
-        // Sluit het menu als er op een link wordt geklikt
         navLinks.querySelectorAll("a").forEach(link => {
             link.addEventListener("click", function () {
                 navLinks.classList.remove("active");
